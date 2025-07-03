e7aa00b912b0fce156668f48fc01e0d3650d03ed
theBook273 <votansach273@gmail.com>
2025-07-03 10:24:30 +0700

    first update

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,30 @@
+return {
+  { "olimorris/onedarkpro.nvim" },
+
+  { "everviolet/nvim" },
+
+  { "xfyuan/nightforest.nvim" },
+
+  { "marko-cerovac/material.nvim" },
+
+  { "ayu-theme/ayu-vim" },
+
+  { "ricardoraposo/nightwolf.nvim" },
+
+  { "rebelot/kanagawa.nvim" },
+
+  { "ellisonleao/gruvbox.nvim" },
+
+  { "sainnhe/gruvbox-material" },
+
+  { "cryptomilk/nightcity.nvim" },
+
+  { "sainnhe/everforest" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "evergarden-fall",
+    },
+  },
+}
